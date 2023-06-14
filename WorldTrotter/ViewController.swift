5ce8187f803db7afd9ac5e735df180c07dae5975
Harrison Anthony <harrisoneanthony@gmail.com>
2023-06-14 00:11:47 -0400

    added map and convert icons and labels

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -9,10 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblFahrenheit: UILabel!
+    @IBOutlet weak var lblCelsius: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        self.lblFahrenheit.text = "100"
+        self.lblCelsius.text = "60"
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [UIColor.blue.cgColor, UIColor.red.cgColor]
+        
+        view.layer.insertSublayer(gradientLayer, at: 0)
+        }
 
 
 }
