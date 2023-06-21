2fc008f002853e63372b8c4ba0143ed8ed940e8e
Harrison Anthony <harrisoneanthony@gmail.com>
2023-06-20 21:41:49 -0400

    added updateCelciusLabel to override viewdidload, added numberFormatter for managing fractional values

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -13,13 +13,39 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var lblCelsius: UILabel!
     @IBOutlet weak var textField: UITextField!
     
+    // declaring temp optionals for conversion
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        //add property ovserver
+        didSet {
+            updateCelciusLabel()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    // add number formatter as a property
+    let nummberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("ConversionViewController loaded its view")
-        self.txtFahrenheit.text = ""
-        self.txtFahrenheit.delegate = self
-        self.lblCelsius.text = "60"
         
+        print("ConversionViewController loaded its view")
+        
+        //override viewDidLoad() with updateCelsiusLabel to set initial value of celsius to "???"
+        updateCelciusLabel()
+                
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = view.bounds
         gradientLayer.colors = [UIColor.blue.cgColor, UIColor.red.cgColor]
@@ -29,10 +55,10 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
     
     // functions for fahrenheit and celcius txt
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField){
-        if let text = textField.text, !text.isEmpty {
-            lblCelsius.text = text
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
         } else {
-            lblCelsius.text = "???"
+            fahrenheitValue = nil
         }
     }
     
@@ -49,6 +75,14 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
         textField.resignFirstResponder()
     }
     
+    //updating celsius label
+    func updateCelciusLabel(){
+        if let celsiusValue = celsiusValue {
+            lblCelsius.text = "\(celsiusValue.value)"
+        } else {
+            lblCelsius.text = "???"
+        }
+    }
     
 }
 
